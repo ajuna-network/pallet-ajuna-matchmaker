47424bf0bd01914a14c3318b2704785ea8d82839
Cedric Decoster <cedric.decoster@gmail.com>
2021-10-25 16:00:15 +0200

    made it instantiable

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 
 pub use pallet::*;
 
-use codec::{Decode, Encode};
+use codec::{Decode, Encode, MaxEncodedLen};
 use sp_std::boxed::{
 	Box
 };
@@ -40,7 +40,7 @@ pub enum MatchingType {
 	Mix,
 }
 
-#[derive(Encode, Decode, Default, Clone, PartialEq, Eq, TypeInfo)]
+#[derive(Encode, Decode, Default, Clone, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct PlayerStruct<AccountId> {
 	account: AccountId,
@@ -48,18 +48,17 @@ pub struct PlayerStruct<AccountId> {
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*};
 
 	// important to use outside structs and consts
 	use super::*;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config<I: 'static = ()>: frame_system::Config {
 
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Event: From<Event<Self, I>> + IsType<<Self as frame_system::Config>::Event>;
 
 		/// Constant that indicates how many players are need to create a new match.
 		#[pallet::constant]
@@ -72,45 +71,35 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
-
-	// The pallet's runtime storage items.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::generate_storage_info]
+	pub struct Pallet<T, I = ()>(_);
 
 	#[pallet::type_value]
-	pub fn BracketsCountDefault<T: Config>() -> u8 { T::AmountBrackets::get() }
+	pub fn BracketsCountDefault<T: Config<I>, I: 'static>() -> u8 { T::AmountBrackets::get() }
 	#[pallet::storage]
 	#[pallet::getter(fn brackets_count)]
-	pub type BracketsCount<T> = StorageValue<_, u8, ValueQuery, BracketsCountDefault<T>>;
+	pub type BracketsCount<T: Config<I>, I: 'static = ()> = StorageValue<_, u8, ValueQuery, BracketsCountDefault<T, I>>;
 
 	// Default value for Nonce
 	#[pallet::type_value]
-	pub fn BracketIndicesDefault<T: Config>() -> (BufferIndex, BufferIndex) { (0, 0) }
+	pub fn BracketIndicesDefault<T: Config<I>, I: 'static>() -> (BufferIndex, BufferIndex) { (0, 0) }
 	#[pallet::storage]
 	#[pallet::getter(fn indices)]
-	pub type BracketIndices<T: Config> = StorageMap<_, Blake2_128Concat, Bracket, (BufferIndex, BufferIndex), ValueQuery, BracketIndicesDefault<T>>;
+	pub type BracketIndices<T: Config<I>, I: 'static = ()> = StorageMap<_, Blake2_128Concat, Bracket, (BufferIndex, BufferIndex), ValueQuery, BracketIndicesDefault<T, I>>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn index_key)]
-	pub type BracketIndexKeyMap<T: Config> = StorageDoubleMap<_, Blake2_128Concat, Bracket, Blake2_128Concat, BufferIndex, T::AccountId, ValueQuery>;
+	pub type BracketIndexKeyMap<T: Config<I>, I: 'static = ()> = StorageDoubleMap<_, Blake2_128Concat, Bracket, Blake2_128Concat, BufferIndex, T::AccountId, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn key_value)]
-	pub type BracketKeyValueMap<T: Config> = StorageDoubleMap<_, Blake2_128Concat, Bracket, Blake2_128Concat, T::AccountId, PlayerStruct<T::AccountId>, ValueQuery>;
+	pub type BracketKeyValueMap<T: Config<I>, I: 'static = ()> = StorageDoubleMap<_, Blake2_128Concat, Bracket, Blake2_128Concat, T::AccountId, PlayerStruct<T::AccountId>, ValueQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://substrate.dev/docs/en/knowledgebase/runtime/events
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+	pub enum Event<T: Config<I>, I: 'static = ()> {
 		/// Queued event
 		Queued(PlayerStruct<T::AccountId>),
 		/// Popped event
@@ -119,76 +108,22 @@ pub mod pallet {
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
-	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+	pub enum Error<T, I = ()> {
 		/// Queue size is to low.
 		QueueSizeToLow,
 		/// Queue is empty.
 		QueueIsEmpty,
 	}
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-		// `on_initialize` is executed at the beginning of the block before any extrinsic are
-		// dispatched.
-		//
-		// This function must return the weight consumed by `on_initialize` and `on_finalize`.
-		fn on_initialize(_: T::BlockNumber) -> Weight {
-			// Anything that needs to be done at the start of the block.
-			// We don't do anything here.
-			0
-		}
-	}
-
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
-	impl<T:Config> Pallet<T> {
-
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
 	}
 }
 
-impl<T: Config> Pallet<T> {
+impl<T: Config<I>, I: 'static> Pallet<T, I> {
 
 	/// Constructor function so we don't have to specify the types every time.
 	///
